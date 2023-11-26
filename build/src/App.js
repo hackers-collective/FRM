a21917e95cd3898a52b6a04a862c0f19a782bdf8
Shobhit Sharma <shobhit0812@gmail.com>
2023-11-27 00:01:04 +0530

    Update App.js

--- build/src/App.js
+++ build/src/App.js
@@ -1,35 +1,41 @@
-import React from 'react';
-
-// TXNs API/Query
-
-const txns = [
-  { mode: 'UPI', isOTP: false, id: 138292 },
-  { mode: 'WALLET', isOTP: true, id: 229383 },
-  { mode: 'ECOM', isOTP: true, id: 200393 },
-];
+import React, { useState, useEffect } from 'react';
 
 export default function Transactions() {
-  const txnList = txns.map(txn =>
-    
-    <>
-    <h1 key={txn.id}
-      style={
-        {
-          color: txn.isOTP ? 'red' : 'darkgreen',
-          fontStyle: txn.isOTP ? 'italic' : ''
+  const [txns, setTxns] = useState([]);
+
+  useEffect(() => {
+    // Fetch data when the component mounts
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'https://raw.githubusercontent.com/scriptkkiddie/Dataset/main/API/FinTech/Transactions.json'
+        );
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
         }
+
+        const data = await response.json();
+        setTxns(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures the effect runs only once, similar to componentDidMount
+
+  const txnList = txns.map(txn => (
+    <h1
+      key={txn.id}
+      style={{
+        color: txn.isOTP ? 'red' : 'darkgreen',
+        fontStyle: txn.isOTP ? 'italic' : '',
+      }}
     >
       {txn.mode}
     </h1>
-{/*     <br /> */}
-      </>
-      
-  );
+  ));
 
-  return (
-    <>
-    {txnList}
-    </>
-  );
+  return <>{txnList}</>;
 }
