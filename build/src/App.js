da3a45dcb86174cba39f7639fc1fdc8816444a54
Shobhit Sharma <shobhit0812@gmail.com>
2023-11-28 23:49:50 +0530

    Update App.js

--- build/src/App.js
+++ build/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Header from './Header';
 import TransactionTable from './TransactionTable';
+import FraudForm from './FraudForm';
 
 export default function App() {
+
+  const [txns, setTxns] = useState([]);
+
+  const handleReport = (fraudData) => {
+    
+    setTxns([...txns, fraudData]);
+  };
   return (
     <div>
       <Header />
+      <FraudForm onReport={handleReport} />
       <TransactionTable />
     </div>
   );
