4b1f1e2b5402124e4ec4ec74b6b1fcabde8405e8
Shobhit Sharma <shobhit0812@gmail.com>
2023-11-28 23:51:07 +0530

    Update TransactionTable.js

--- build/src/TransactionTable.js
+++ build/src/TransactionTable.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-function TransactionTable() {
+function TransactionTable({ txns }) {
   const [txns, setTxns] = useState([]);
   const [error, setError] = useState(null);
 
