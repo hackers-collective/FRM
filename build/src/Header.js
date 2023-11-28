58e9f4b5075f7fbd71d305f5ec569985053512df
Shobhit Sharma <shobhit0812@gmail.com>
2023-11-28 23:05:49 +0530

    Create Header.js

---
+++ build/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+
+  return (
+    <header>
+      <div className="logo">
+        <h1>Fraud Risk Management</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
