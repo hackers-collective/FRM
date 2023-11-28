839275971c6c0fd4f29ee0878655344b067f9cc6
Shobhit Sharma <shobhit0812@gmail.com>
2023-11-29 00:00:53 +0530

    Update App.js

--- build/src/App.js
+++ build/src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import TransactionTable from './TransactionTable';
 import FraudForm from './FraudForm';
 
 export default function App() {
-
   const [txns, setTxns] = useState([]);
 
   const handleReport = (fraudData) => {
-    
+    // Update state with the new fraud data
     setTxns([...txns, fraudData]);
   };
+
   return (
     <div>
       <Header />
       <FraudForm onReport={handleReport} />
-      <TransactionTable />
+      <TransactionTable transactions={txns} />
     </div>
   );
 }
