938aa34b69f9f54c9624cc33c4a0342b136c57bd
Shobhit Sharma <shobhit0812@gmail.com>
2023-11-28 23:22:07 +0530

    Update TransactionTable.js

--- build/src/TransactionTable.js
+++ build/src/TransactionTable.js
@@ -21,7 +21,29 @@ function TransactionTable() {
   if (error) {
     return <div>Error: {error}</div>;
   }
-  
+
+  const txnList = txns.map((txn, index) => (
+    <tr key={index}>
+      <td>{txn.txnMode}</td>
+      <td>{txn.refUserNo}</td>
+      <td>{txn.isOTP ? 'true' : 'false'}</td>
+      <td>{txn.status}</td>
+     </tr>
+  ));
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Mode</th>
+          <th>TXN ID</th>
+          <th>OTP Triggered?</th>
+          <th>TXN Status</th>
+        </tr>
+      </thead>
+      <tbody>{txnList}</tbody>
+    </table>
+  );
 }
 
 export default TransactionTable;
