728c0e548d7db74ab6413f91ca480522cf658834
Shobhit Sharma <shobhit0812@gmail.com>
2023-11-29 23:32:54 +0530

    Create CategoryTransactions.js

---
+++ build/src/CategoryTransactions.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function CategoryTransactions({ transactions, category}) {
+
+  const filteredTransactions = transaction.filter(
+    (txn) => txn.txnMode.toLowerCase() === category.toLowerCase()
+  );
+
+  return (
+    <div>
+      <h2>{category} Transactions</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>TXN ID</th>
+            <th>OTP Triggered?</th>
+            <th>TXN Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredTransactions.map((txn, index) => (
+            <tr key={index}>
+              <td>{txn.refUserNo}</td>
+              <td>{txn.isOTP ? 'true' : 'false'}</td>
+              <td>{txn.status}</td>
+              <td>{txn.isFraudulent ? 'Fraudulent' : 'Not Fraudulent'}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default CategoryTransactions;
