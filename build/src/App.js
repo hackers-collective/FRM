312c8a09b9fa4ff5576c9f365ae15500a08f6f1b
Shobhit Sharma <shobhit0812@gmail.com>
2023-12-04 18:52:21 +0530

    Update App.js

--- build/src/App.js
+++ build/src/App.js
@@ -1,21 +1,10 @@
 import React, { useState } from 'react';
-import Header from './Header';
-import TransactionTable from './TransactionTable';
-import FraudForm from './FraudForm';
 
 export default function App() {
-  const [txns, setTxns] = useState([]);
 
-  const handleReport = (fraudData) => {
-    const newFraudData = { ...fraudData, isFraudulent: false };
-    setTxns([...txns, newFraudData]);
-  };
+  const title = 'Fraud Risk Management';
 
   return (
-    <div>
-      <Header />
-      <FraudForm onReport={handleReport} />
-      <TransactionTable transactions={txns} />
-    </div>
+     <h1>{title}</h1>
   );
 }
