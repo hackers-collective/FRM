1cbcbc3956265a6f8094ba79e1345e1a7f2d4b6e
Shobhit Sharma <shobhit0812@gmail.com>
2023-12-04 18:40:52 +0530

    Update index.js

--- build/src/index.js
+++ build/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
+import Report from './Report';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,6 +10,7 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/home" element={<App />} />
+        <Route path="/report" element={<Report />}
       </Routes>
     </Router>
   </React.StrictMode>,
